10af85e17199ccbc4b6f957e6f839183c8533bc0
mayur70 <mayurpatil70@gmail.com>
2025-04-15 21:16:19 +0530

    template parsing

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.tmpl")
 	router.GET("/", welcome)
 	router.GET("/oups", triggerException)
 	router.GET("/vets", showResourcesVetList)
+	router.GET("/vets.html", showVetList)
 
 	router.Run("localhost:8080")
 }
@@ -20,6 +23,10 @@ type Vet struct {
 	LastName  string `json:"lastName"`
 }
 
+var vets = []Vet{
+	{ID: 1, FirstName: "Mayur", LastName: "Patil"},
+}
+
 func welcome(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "welcome"})
 }
@@ -28,9 +35,28 @@ func triggerException(c *gin.Context) {
 	//TODO: throw expection??
 }
 
+func showVetList(c *gin.Context) {
+	c.HTML(http.StatusOK, "vets.tmpl", gin.H{
+		"title": "vets page is HERE",
+	})
+}
+
 func showResourcesVetList(c *gin.Context) {
-	vets := []Vet{
-		{ID: 1, FirstName: "Mayur", LastName: "Patil"},
-	}
 	c.IndentedJSON(http.StatusOK, vets)
+}
+
+func findVetByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
+		return
+	}
+
+	for _, v := range vets {
+		if v.ID == id {
+			c.IndentedJSON(http.StatusOK, v)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "vet not found"})
 }
